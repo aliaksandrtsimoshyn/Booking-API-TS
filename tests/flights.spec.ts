6206d6050981253452ab82171f3e300a65cd3b8e
aliaksandrtsimoshyn <aliaksandr.tsimoshyn2@gmail.com>
2024-04-08 14:31:05 +0300

    Update flights.spec.ts to include new flightService methods and book tickets for different flight statuses

--- tests/flights.spec.ts
+++ tests/flights.spec.ts
@@ -1,4 +1,4 @@
-import { settings, testUsers, roles } from '../utils/settings'
+import { settings, testUsers, roles, flightStatuses, fareConditions } from '../utils/settings'
 import { test } from '../utils/fixtures'
 import { createAuthorizedAPIContext } from '../utils/helpers/functions'
 
@@ -15,31 +15,64 @@ test.beforeAll(async ({}) => {
 })
 
 test.describe(`FLIGHTS`, () => {
-  // Test data for Get All Flights test
+  test(`Get Flights`, async ({ flightService }) => {
+    const flightsData = await flightService.getFlights(flightStatuses.scheduled, 200)
+
+    console.log(`The flights:`, flightsData)
+  })
+
+  test(`Get Specific Flight`, async ({ flightService }) => {
+    const specificFlightData = await flightService.getSpecificFlight(flightStatuses.scheduled, 200)
+
+    console.log(`The flight:`, specificFlightData)
+  })
+
+  test(`Get Free Seats`, async ({ flightService }) => {
+    const freeSeatsData = await flightService.getFreeSeats(flightStatuses.scheduled, 200)
+
+    console.log(`Free seats:`, freeSeatsData)
+  })
+
+  // Test data for Book Tickets test
   const testData1 = [
-    { statusCode: 200, authRole: roles.admin },
-    { statusCode: 200, authRole: roles.customer },
+    { flightStatus: flightStatuses.scheduled, responseStatus: 201 },
+    { flightStatus: flightStatuses.arrived, responseStatus: 403 },
+    { flightStatus: flightStatuses.cancelled, responseStatus: 403 },
+    { flightStatus: flightStatuses.delayed, responseStatus: 403 },
+    { flightStatus: flightStatuses.departed, responseStatus: 403 },
+    { flightStatus: flightStatuses.on_time, responseStatus: 403 },
   ]
 
   for (const data of testData1) {
-    test(`Get All Flights By ${data.authRole}`, async ({ flightService }) => {
-      const flightsData = await flightService.getAllFlights(data.statusCode, data.authRole)
+  test(`Book Tickets For ${data.flightStatus} Flight`, async ({ flightService, newCustomer }) => {
+    const requestData = {
+      account_id: newCustomer.user_id,
+      tickets: [
+        {
+          amount: 10,
+          fare_conditions: fareConditions.economy,
+          passenger_name: newCustomer.username,
+          phone: newCustomer.phone_number,
+          email: newCustomer.email,
+        },
+      ],
+    }
+    const bookTicketsData = await flightService.bookTickets(requestData, data.flightStatus, data.responseStatus)
 
-      console.log(`All flights:`, flightsData.objects)
-    })
-  }
+    console.log(`Booking data:`, bookTicketsData)
+  })}
 
   // Test data for Get User Bookings test
   const testData2 = [
-    { statusCode: 200, authRole: roles.admin },
-    { statusCode: 403, authRole: roles.customer },
+    { responseStatus: 200, authRole: roles.admin },
+    { responseStatus: 403, authRole: roles.customer },
   ]
 
   for (const data of testData2) {
     test(`Get User Bookings By ${data.authRole}`, async ({ flightService, newCustomer }) => {
       const bookingsData = await flightService.getUserBookings(
         newCustomer.user_id as string,
-        data.statusCode,
+        data.responseStatus,
         data.authRole
       )
 
